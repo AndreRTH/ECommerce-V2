cfd2f8528ba3cdbc22b08f1c36df84c4c900f896
CtrAltElite <abarrerasprogrammer@gmail.com>
2023-04-06 09:09:54 -0400

    This should be the last commit needed for deployment but who knows really.

--- src/components/page/products/Products.js
+++ src/components/page/products/Products.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Navbar from '../../bars/Navbar';
-// import Productcard from './Productcard';
+import Productcard from './Productcard';
 import './productcard.css';
 import Footer from '../../bars/Footer';
-import Dbcard from './Dbcard';
+// import Dbcard from './Dbcard';
 
 export default function Products() {
   return (
     <>
     <Navbar />
-    <Dbcard />
+    <Productcard />
     <Footer />
     </>
   )
